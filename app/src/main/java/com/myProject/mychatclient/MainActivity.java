ce7398bfc6a0d4b0687926824d8417b9e926f388
elisey654321 <elisey654321@mail.ru>
2021-10-05 22:42:10 +0300

    webSocket это пиздец.
    webSocket это пиздец.

--- app/src/main/java/com/myProject/mychatclient/MainActivity.java
+++ app/src/main/java/com/myProject/mychatclient/MainActivity.java
@@ -1,13 +1,77 @@
 package com.myProject.mychatclient;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import okhttp3.*;
+import okio.ByteString;
+import tech.gusavila92.websocketclient.WebSocketClient;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebSocketClient webSocketClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         createWebSocketClient();
     }
+
+    private void createWebSocketClient(){
+        URI uri;
+        try {
+            // Connect to local host
+            uri = new URI("ws://localhost:8080/gs-guide-websocket/");
+        }
+        catch (URISyntaxException e) {
+            e.printStackTrace();
+            return;
+        }
+        webSocketClient = new WebSocketClient(uri) {
+            @Override
+            public void onOpen() {
+                Log.i("WebSocket", "Session is starting");
+                webSocketClient.send("Hello World!");
+            }
+            @Override
+            public void onTextReceived(String s) {
+                Log.i("WebSocket", "Message received");
+                final String message = s;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                    }
+                });
+            }
+            @Override
+            public void onBinaryReceived(byte[] data) {
+            }
+            @Override
+            public void onPingReceived(byte[] data) {
+            }
+            @Override
+            public void onPongReceived(byte[] data) {
+            }
+            @Override
+            public void onException(Exception e) {
+                System.out.println(e.getMessage());
+            }
+            @Override
+            public void onCloseReceived() {
+                Log.i("WebSocket", "Closed ");
+                System.out.println("onCloseReceived");
+            }
+        };
+        webSocketClient.setConnectTimeout(10000);
+        webSocketClient.setReadTimeout(60000);
+        webSocketClient.enableAutomaticReconnection(5000);
+        webSocketClient.connect();
+    }
+
 }
